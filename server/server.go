7333546681a7cf09a45a37a57421d049c5cb74db
Myriad-Dreamin <camiyoru@gmail.com>
2019-07-01 20:10:41 +0800

    grpc test

---
+++ server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	uiprpc "github.com/Myriad-Dreamin/go-ves/grpc"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const port = ":23351"
+
+type Server struct {
+}
+
+func (server *Server) SayHello(
+	ctx context.Context,
+	in *uiprpc.HelloRequest,
+) (*uiprpc.HelloReply, error) {
+	return &uiprpc.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	uiprpc.RegisterGreeterServer(s, &Server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
