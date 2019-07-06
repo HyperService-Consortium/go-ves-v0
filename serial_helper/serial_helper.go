370e6c0ccda8fe0f37452d919e9657a584a0f41a
Myriad-Dreamin <camiyoru@gmail.com>
2019-07-06 23:50:18 +0800

    serial_helper

--- serial_helper/serial_helper.go
+++ serial_helper/serial_helper.go
@@ -4,30 +4,55 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"io"
 
 	types "github.com/Myriad-Dreamin/go-ves/types"
 )
 
-func SerializeAccountInterface(account types.Account) []byte {
-	var buf = new(bytes.Buffer)
-	var bx = make([]byte, 8)
-	binary.PutUvarint(bx, account.GetChainType())
-	buf.Write(bx)
+var (
+	insufficientBytes = errors.New("insufficient bytes to read")
+)
+
+func SerializeAccountsInterfaceBuffer(buf io.ReadWriter, account types.Account) error {
+	err := binary.Write(buf, binary.LittleEndian, account.GetChainType())
+	if err != nil {
+		return err
+	}
+
 	var bc = account.GetAddress()
-	binary.PutVarint(bx, int64(len(bc)))
-	buf.Write(bx)
+	err = binary.Write(buf, binary.LittleEndian, int64(len(bc)))
+	if err != nil {
+		return err
+	}
 	buf.Write(bc)
-	return buf.Bytes()
+	return nil
+}
+
+func SerializeAccountInterface(account types.Account) ([]byte, error) {
+	var buf = new(bytes.Buffer)
+	err := SerializeAccountsInterfaceBuffer(buf, account)
+	return buf.Bytes(), err
+}
+
+func SerializeAccountsInterface(accounts []types.Account) ([]byte, error) {
+	var buf = new(bytes.Buffer)
+	for _, account := range accounts {
+		err := SerializeAccountsInterfaceBuffer(buf, account)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buf.Bytes(), nil
 }
 
 func UnserializeAccountInterface(b []byte) (n int64, chain_type uint64, address []byte, err error) {
 	var buf = bytes.NewBuffer(b)
 	var ilen int64
-	chain_type, err = binary.ReadUvarint(buf)
+	err = binary.Read(buf, binary.LittleEndian, &chain_type)
 	if err != nil {
 		return
 	}
-	ilen, err = binary.ReadVarint(buf)
+	err = binary.Read(buf, binary.LittleEndian, &ilen)
 	if err != nil {
 		return
 	}
@@ -39,9 +64,18 @@ func UnserializeAccountInterface(b []byte) (n int64, chain_type uint64, address 
 	}
 	n = int64(nn)
 	if n < ilen {
-		err = errors.New("insufficient bytes to read")
+		err = insufficientBytes
 		return
 	}
 	n += 16
 	return
+}
+
+func DecoratePrefix(pre, b []byte) ([]byte, error) {
+	var buf = bytes.NewBuffer(pre)
+	_, err := buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
---
+++ serial_helper/serial_helper_test.go
@@ -0,0 +1,118 @@
+package SerialHelper
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	types "github.com/Myriad-Dreamin/go-ves/types"
+	mtest "github.com/Myriad-Dreamin/mydrest"
+)
+
+var s mtest.TestHelper
+
+type mA struct {
+	x uint64
+	y []byte
+}
+
+func (a *mA) GetChainType() uint64 { return a.x }
+func (a *mA) GetAddress() []byte   { return a.y }
+
+func TestSerialize(t *testing.T) {
+	a := &mA{1, []byte{6, 6}}
+
+	qwq, err := SerializeAccountInterface(a)
+	s.AssertNoErr(t, err)
+
+	var b = new(mA)
+	var n int64
+	n, b.x, b.y, err = UnserializeAccountInterface(qwq)
+	s.AssertNoErr(t, err)
+	fmt.Println(n, len(qwq), a, b)
+
+	s.AssertEqual(t, a.x, b.x)
+
+	s.AssertTrue(t, bytes.Equal(a.y, b.y))
+}
+
+func TestInsufficientBytesError(t *testing.T) {
+	a := &mA{1, []byte{6, 6}}
+
+	qwq, err := SerializeAccountInterface(a)
+	s.AssertNoErr(t, err)
+
+	qwq = qwq[:len(qwq)-1]
+	var b = new(mA)
+	_, b.x, b.y, err = UnserializeAccountInterface(qwq)
+	s.AssertEqual(t, err, insufficientBytes)
+}
+
+func TestTwoAccounts(t *testing.T) {
+	a, b := &mA{1, []byte{6, 6}}, &mA{2, []byte{6, 2, 2, 2, 3, 4, 6}}
+
+	var buf = new(bytes.Buffer)
+	qwq, err := SerializeAccountInterface(a)
+	s.AssertNoErr(t, err)
+	_, err = buf.Write(qwq)
+	s.AssertNoErr(t, err)
+	qwq, err = SerializeAccountInterface(b)
+	s.AssertNoErr(t, err)
+	_, err = buf.Write(qwq)
+	s.AssertNoErr(t, err)
+	qwq = buf.Bytes()
+
+	var c = new(mA)
+	var n int64
+	n, c.x, c.y, err = UnserializeAccountInterface(qwq)
+	s.AssertNoErr(t, err)
+
+	fmt.Println(n, len(qwq), a, c)
+	s.AssertEqual(t, a.x, c.x)
+	s.AssertTrue(t, bytes.Equal(a.y, c.y))
+
+	qwq = qwq[n:]
+
+	n, c.x, c.y, err = UnserializeAccountInterface(qwq)
+	s.AssertNoErr(t, err)
+
+	fmt.Println(n, len(qwq), b, c)
+	s.AssertEqual(t, b.x, c.x)
+	s.AssertTrue(t, bytes.Equal(b.y, c.y))
+
+	qwq = qwq[n:]
+
+	n, c.x, c.y, err = UnserializeAccountInterface(qwq)
+	fmt.Println(n, len(qwq), c)
+	s.AssertEqual(t, err, io.EOF)
+}
+
+func TestThreeAccounts(t *testing.T) {
+	a := []types.Account{
+		&mA{1, []byte{6, 6}},
+		&mA{2, []byte{6, 2, 2, 2, 3, 4, 6}},
+		&mA{1, []byte{6, 6}},
+		&mA{0xaa00aa00, []byte{0xa0, 0x0a, 0x5a, 0xa5, 0x50, 0x05}},
+		&mA{0xaa0000aa, []byte{0xa0, 0x0a, 0x5a, 0xa5, 0x50, 0x05}},
+	}
+
+	qwq, err := SerializeAccountsInterface(a)
+	s.AssertNoErr(t, err)
+
+	var c = new(mA)
+	var n int64
+	for _, aa := range a {
+		n, c.x, c.y, err = UnserializeAccountInterface(qwq)
+		s.AssertNoErr(t, err)
+
+		fmt.Println(n, len(qwq), aa, c)
+		s.AssertEqual(t, aa.GetChainType(), c.x)
+		s.AssertTrue(t, bytes.Equal(aa.GetAddress(), c.y))
+		qwq = qwq[n:]
+	}
+
+	n, c.x, c.y, err = UnserializeAccountInterface(qwq)
+	fmt.Println(n, len(qwq), c)
+	s.AssertEqual(t, err, io.EOF)
+}
